4fd68b7e7214348b44e290ace92fb1416a7a1756
Floppsopp <lukasjoneklint@gmail.com>
2025-10-23 09:40:49 +0200

    playerMovementScript

--- Electric_DigDig02_Spelprojekt/Assets/Scripts/playerMovement.cs
+++ Electric_DigDig02_Spelprojekt/Assets/Scripts/playerMovement.cs
@@ -1,70 +1,79 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    CharacterController controller;
-    public Transform camera1;
 
-    float moveInput;
-    float turnInput;
-    public float walkSpeed = 5f;
-    public float turnSpeed = 2f;
+    public CharacterController controller;
+    public Transform cam;
+    
+    // Player speed
+    public float Speed = 5f;
+
+    // smooth turning for the character
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    // Jumpheight for the player
     float jumpHeight = 1f;
 
+    // gravity and velocity to make the player be grounded when not jumping 
     float gravity = 9.82f;
     public float verticalVelocity;
 
+    // Checks if the player is touching the ground.
     public bool Grounded;
-
-    void Start()
-    {
-        controller = GetComponent<CharacterController>();
-    }
 
     void Update()
     {
-        InputManagement();
         Movement();
-        Turn();
         VerticalForce();
     }
 
-    void InputManagement()
-    {
-        moveInput = Input.GetAxis("Vertical");
-        turnInput = Input.GetAxis("Horizontal");
-    }
-
+    //Movement script
     void Movement()
     {
-        Vector3 move = new Vector3(turnInput, 0, moveInput);
+        //Assigns axis for the player movement (not jump).
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        // player move speed is normalized and doesn't accelerate.
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        //for the Vertical velocity to work
+        Vector3 move = Vector3.zero;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            //Movement and smooth turning
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            //Makes the player move smoother when turning.
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            //changes player direction. ex, player turns left is going to face left.
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * Speed * Time.deltaTime);
+        }
+        // verticalVelocity getting assigned to y cordinate
         move.y = verticalVelocity;
-        move *= walkSpeed;
+
         controller.Move(move * Time.deltaTime);
     }
 
-    void Turn()
-    {
-        if (Mathf.Abs(turnInput) > 0 || Mathf.Abs(moveInput) > 0)
-        {
-            Vector3 currentLookDirection = camera1.forward;
-            currentLookDirection.y = 0;
-
-            Quaternion targetRotation = Quaternion.LookRotation(currentLookDirection);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
-        }
-    }
-
+    //Code for jump function
     float VerticalForce()
     {
         if (controller.isGrounded)
         {
+            // If grounded the vertical velocity is kept at -1f so it stays on the ground 
             verticalVelocity = -1f;
             Grounded = true;
+            // When the button space ("Jump") is pressed the player jumps.
             if (Input.GetButtonDown("Jump"))
             {
+                // Equation for jumping
                 verticalVelocity = Mathf.Sqrt(jumpHeight * gravity * 2);
             }
         }
