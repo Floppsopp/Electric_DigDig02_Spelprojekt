d4198bf86db36216eaa41c8f327224f083e5a691
Floppsopp <lukasjoneklint@gmail.com>
2025-10-23 11:16:08 +0200

    comments in dealDamage Script

--- Electric_DigDig02_Spelprojekt/Assets/Scripts/dealDamage.cs
+++ Electric_DigDig02_Spelprojekt/Assets/Scripts/dealDamage.cs
@@ -9,8 +9,10 @@ public class dealDamage : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        // Compares the tag on collision (Must match Enemy_1)
         if(other.CompareTag("Enemy_1"))
         {
+            //Gets component from enemy1Script to be able to deal damage.
             enemy1Script enemy = other.GetComponent<enemy1Script>();
             enemy.TakeDamage(damage);
         }
